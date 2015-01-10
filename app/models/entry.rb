8d04a9d09d0aac644ba9211c44f3b7c7212f8680
Min McKnight <min.mcknight@gmail.com>
2015-01-10 17:41:21 -0500

    picture successully created

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,14 @@
 class Entry < ActiveRecord::Base
+  validates_presence_of :author
+  validates_presence_of :photo_url
+  validates_presence_of :date_taken
+  validate :validate_date_taken
+
+def validate_date_taken
+  if date_taken < Date.new(2010,10,1)
+    errors.add(:taken_date, "is older than 10/1/2010")
+  end
 end
+end
+
+
